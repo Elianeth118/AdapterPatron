53906e38dd5c81fc6fb6317bd9ff65e0335d6e5d
Elianeth118 <aragonelianeth@gmail.com>
2024-05-29 22:51:39 -0600

    Agregar archivos de proyecto.

---
+++ Adapter.Ejemplo1/NewSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter.Ejemplo1
+{
+    public class NewSystem
+    {
+        private readonly IDatabase _database;
+        public  NewSystem(IDatabase database)
+        {
+            _database = database;
+        }
+        public void SaveData(string data)
+        {
+            _database.Add(data);
+            Console.WriteLine("Datos guardados en el nuevo sistema");
+
+        }
+        public void LoadData()
+        {
+            string data = _database.Retrieve();
+            Console.WriteLine("Data loaded in thenew system");
+        }
+    }
+}
---
+++ Adapter.Ejemplo1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Adapter.Ejemplo1;
+
+LegacyDatabase legacyDatabase = new LegacyDatabase();
+DatabaseAdapter adapter = new DatabaseAdapter(legacyDatabase);
+NewSystem newSystem=new NewSystem(adapter);
+newSystem.SaveData("Impostant data");
+newSystem.LoadData();
